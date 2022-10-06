47413b1ea3d45e9b030f2c5880e730dab21f3cda
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-10-06 16:51:01 +0200

    feat (backend): add get and post

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express';
+import {getPosts, createPost} from "../controllers/posts.js";
 
 const router = express.Router();
 
 //localhost:3000/posts
-router.get('/', (req, res) => {
-    res.send('SENT!');
-});
+router.get('/', getPosts);
+router.get('/', createPost);
 
 export default router; // to import that router on other side (index.js)
