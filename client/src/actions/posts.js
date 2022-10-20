d00faf5619c248f4bfd11924d6a353ed7850d6b3
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-10-20 17:01:31 +0200

    feat: add forms

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -7,7 +7,7 @@ export const getPosts = () => async (dispatch) => {
     try {
         //patch an action the data from backend
         const {data} = await api.fetchPosts();
-        dispatch({type: 'FETCH_ALL', payload: data}); //return action
+        dispatch({type: 'FETCH_ALL', payload: data}); //return  action
     } catch (error) {
         console.log(error.message);
     }
